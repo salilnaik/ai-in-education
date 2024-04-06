530918d6cbe27fc036ccbb926ac0147580139e74
Salil Naik <salilnaik2004@gmail.com>
2024-04-05 20:13:59 -0700

    added home screen

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
+def home():
+    return '<a href="/chat">Chat</a>'
+
+@app.route("/chat")
 def hello_world():
     return render_template('index.html')
 
