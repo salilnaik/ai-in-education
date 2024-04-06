1c258d8f1747de520a7c52c21ad265ff6f626355
Salil Naik <salilnaik2004@gmail.com>
2024-04-05 20:45:09 -0700

    added interface between frontend and backend to send messages

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
-    return '<a href="/chat">Chat</a>'
+    # return '<a href="/chat">Chat</a>'
+    return render_template('home.html')
 
 @app.route('/chat', methods=['POST', 'GET'])
 def chat():
